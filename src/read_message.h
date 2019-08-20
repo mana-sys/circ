70845cbc28223f86e0a76a680a3b196f0d9b1564
benlalanes <bllanes@ucsd.edu>
2019-08-19 19:54:55 -0700

    Add conn_read_message() function.

--- src/read_message.h
+++ src/read_message.h
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#include "connection.h"
+
 /**
  * Returns a pointer to the first occurrence of the CRLF sequence within the
  * specified string, up to the specified limit. If the CRLF sequence is not
@@ -16,6 +18,20 @@
 char *strncrlf(char *str, size_t limit);
 
 
-ssize_t read_message(int fd, char *msg, char *buf, ssize_t *storeTotalRead, bool *storeDiscardNext);
+ssize_t read_message      (int fd, char *msg, char *buf, ssize_t *storeTotalRead, bool *storeDiscardNext);
+
+/**
+ * Attempt to perform a message read for the specified connection based
+ * on the state of the connection. If the connection's flag to discard
+ * the next message is set, then this function will discard at most one
+ * existing message, if it finds one, and will reattempt the read.
+ *
+ * @param conn The connection on which to perform a message read on.
+ * @return A pointer to the message that was read, or a null pointer if no
+ * message could be read.
+ */
+// TODO: Handle corner cases:
+//   - Message of length 513 (i.e. message[511] = '\r', message[512] = '\n'
+char * conn_read_message (conn_s *conn);
 
 #endif
